1a96c973653b03ac8f3598d4d7dcfd95565ef54d
Your Name <you@example.com>
2019-02-17 02:44:53 +0300

    refactor: api response

--- src/Model/ApiResponse.php
+++ src/Model/ApiResponse.php
@@ -11,48 +11,129 @@ namespace App\Model;
 
 use App\Exception\ClassException;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiResponse extends JsonResponse
 {
     /**
+     * @var array|null
+     */
+    private $apiData;
+
+    /**
+     * @var Error|null
+     */
+    private $error;
+
+    /**
+     * @var bool
+     */
+    private $success;
+
+    /**
      * ApiResponse constructor.
-     * @param mixed $data
+     * @param array|null $apiData
      * @param Error|null $error
      * @param bool $success
      * @param int $status
      * @param array $headers
-     * @param bool $json
-     * @throws ClassException
      */
-    public function __construct($data = null,
+    public function __construct(?array $apiData = null,
                                 ?Error $error = null,
                                 bool $success = true,
+                                int $status = 200,
                                 array $headers = array())
     {
-        $apiData = [
-            'success' => $success,
-            'error' => $error ? $error->toArray() : null,
-            'data' => $data
-        ];
-        parent::__construct($apiData, 200, $headers, false);
+        parent::__construct("null", $status, $headers, true);
+
+        $this->apiData = $apiData;
+        $this->error = $error;
+        $this->success = $success;
     }
 
     /**
-     * @param array|null $data
+     * @return array|null
+     */
+    public function getApiData(): ?array
+    {
+        return $this->apiData;
+    }
+
+    /**
+     * @param array|null $apiData
+     * @return ApiResponse
+     */
+    public function setApiData(?array $apiData): ApiResponse
+    {
+        $this->apiData = $apiData;
+        return $this;
+    }
+
+    /**
+     * @return Error|null
+     */
+    public function getError(): ?Error
+    {
+        return $this->error;
+    }
+
+    /**
      * @param Error|null $error
+     * @return ApiResponse
+     */
+    public function setError(?Error $error): ApiResponse
+    {
+        $this->error = $error;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    /**
      * @param bool $success
      * @return ApiResponse
+     */
+    public function setSuccess(bool $success): ApiResponse
+    {
+        $this->success = $success;
+        return $this;
+    }
+
+    /**
+     * @return string
      * @throws ClassException
      */
-    public function setApiData(?array $data = null,
-                               ?Error $error = null,
-                               bool $success = true)
+    public function getContent(): string
     {
-        $apiData = [
-            'success' => $success,
-            'error' => $error ? $error->toArray() : null,
-            'data' => $data
+        $this->setData($this->toArray());
+        return parent::getContent();
+    }
+
+    /**
+     * @return Response
+     * @throws ClassException
+     */
+    public function sendContent()
+    {
+        $this->setData($this->toArray());
+        return parent::sendContent();
+    }
+
+    /**
+     * @return array
+     * @throws ClassException
+     */
+    public function toArray(): array {
+        return [
+            'success' => $this->success,
+            'error' => $this->error ? $this->error->toArray() : null,
+            'data' => $this->apiData
         ];
-        return $this->setData($apiData);
     }
 }
